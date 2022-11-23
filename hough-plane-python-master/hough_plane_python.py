62a97b0bc181f5ce267aee0287fd65e821ce9de4
nico0001 <nicolasjadoul0001@gmail.com>
2022-11-23 11:08:04 +0100

    Rangement Git

--- hough-plane-python-master/hough_plane_python.py
+++ hough-plane-python-master/hough_plane_python.py
@@ -2,6 +2,8 @@ import numpy as np
 import open3d as o3d
 import math
 import logging
+import vizualization as viz
+import matplotlib.pyplot as plt
 logger = logging.getLogger('Hough Plane')
 
 CAN_USE_TQDM = False
@@ -12,16 +14,22 @@ except:
     pass
 
 def _calc_normals(fis_deg, thetas_deg):
-    '''Compute 2d array of vectors with length 1,
-    each representing direction at angles φ and θ in spherical coordinate system'''
+    '''
+    input: 
+        fis_deg         : list of fi in degree
+        thetas_deg      : list of theta in degree
+    Compute 2d array of vectors with length 1,
+    each representing direction at angles φ and θ in spherical coordinate system
+    
+    '''
 
-    fis_len = len(fis_deg)
+    fis_len    = len(fis_deg)
     thetas_len = len(thetas_deg)
 
-    normals = np.zeros((fis_len, thetas_len, 3), dtype=np.float)
+    normals = np.zeros((fis_len, thetas_len, 3), dtype=np.float64)
 
-    fis_rad = fis_deg / 180 * np.pi
-    thetas_rad = thetas_deg / 180 * np.pi
+    fis_rad    = fis_deg    *(np.pi/180)
+    thetas_rad = thetas_deg *(np.pi/180)
 
     for i in range(fis_len):
         fi = fis_rad[i]
@@ -37,7 +45,7 @@ def _calc_normals(fis_deg, thetas_deg):
     return normals
 
 def _dot_prod(point,normals):
-    '''For one point compute projections of this point to all vectors in array "normals"'''
+    '''For one point compute projections of this point to all vectors in array "normals (cartesian)"'''
 
     x,y,z = point
     xx,yy,zz = normals[:,:,0], normals[:,:,1], normals[:,:,2]
@@ -139,71 +147,106 @@ def hough_planes(points, threshold, use_tqdm=True,
     assert(depth_start_step >= 0 and type(depth_start_step) == int)
 
 
-    fis = np.arange(fi_bounds[0], fi_bounds[1], fi_step)
-    thetas = np.arange(theta_bounds[0], theta_bounds[1], theta_step)
-
-    fis_len = len(fis)
+    fis        = np.arange(fi_bounds[0]   , fi_bounds[1]   , fi_step   )
+    thetas     = np.arange(theta_bounds[0], theta_bounds[1], theta_step)
+    fis_len    = len(fis)
     thetas_len = len(thetas)
-    accum = np.zeros([fis_len, thetas_len, depth_steps], dtype=np.int)
+
+    accum   = np.zeros([fis_len, thetas_len, depth_steps], dtype=np.int64)
     normals = _calc_normals(fis, thetas)
 
     depth_bounds = list(depth_bounds)
+
     if depth_bounds[0] > 0:
-        mask = vectors_len(points) > depth_bounds[0]
+        #points = 3D points as numpy array with shape=(?, 3) and dtype=np.float
+        mask = vectors_len(points) > depth_bounds[0] 
         points = points[mask]
 
     if depth_bounds[1] is None:
-        depth_bounds[1] = np.max(points) * 2
+        # points = [[1,2,3],[4,5,6],[7,8,-9]]
+        # print(np.max(points)) = 8 Erreur? il aurait peut être du mettre np.abs()?
+        depth_bounds[1] = 2*np.max(np.abs(points))
+
+
     logger.debug(f'depth_bounds: {depth_bounds}')
+    #depth_steps = nbre of value in accumulator
 
-    depth_skipped_steps = depth_bounds[0] / (depth_bounds[1] - depth_bounds[0]) * depth_steps
-    depth_total_steps = depth_steps + depth_skipped_steps
-    points_scaled = points / depth_bounds[1] * depth_total_steps
+    #ex: total = [0,5], depth_bounds = [2,5]. I know i do depth_steps for the interval [2,5]. How many steps do I do for the interval [0,2]?
+    depth_skipped_steps = depth_steps *((depth_bounds[0] - 0) / (depth_bounds[1] - depth_bounds[0])) 
+    depth_total_steps   = depth_steps + depth_skipped_steps
 
-    fi_idxes = np.zeros([fis_len, thetas_len], dtype=np.int)
-    for i in range(len(fis)):
-        fi_idxes[i] = i
+    points_scaled       = points / depth_bounds[1] * depth_total_steps
+
+    fi_idxes = np.zeros([fis_len, thetas_len], dtype=np.int64)
+    for i in range(len(fis)): #for i in range fis_len 
+        fi_idxes[i] = i #fi_idxes[i,:]
     fi_idxes = fi_idxes.flatten()
-    theta_idxes = np.zeros([fis_len, thetas_len], dtype=np.int)
+    # exemple: 
+    # fis_len =3 , thetas_len=2  => fi_idxes   = [0 0 1 1 2 2]
+    #                            => theta_idxes= [0 1 0 1 0 1]
+
+    #il aurait pu utiliser un scatter entre autre. ça aurait été plus simple.
+
+    theta_idxes = np.zeros([fis_len, thetas_len], dtype=np.int64)
     for i in range(len(thetas)):
         theta_idxes[:, i] = i
     theta_idxes = theta_idxes.flatten()
 
+
     iterator = range(0, len(points))
+    print("len points",len(points))
     if CAN_USE_TQDM and use_tqdm:
         iterator = tqdm(iterator)
     for k in iterator:
+        # point \in [0, depth_step]
         point = points_scaled[k]
+        #For one point compute projections of this point to all planes 
+        dists = _dot_prod(point, normals) - depth_skipped_steps 
 
-        dists = _dot_prod(point, normals) - depth_skipped_steps
-        dists = dists.astype(np.int)
+        dists = dists.astype(np.int64)
         dists = dists.flatten()
 
-        mask = (dists >= 0) * (dists < depth_steps)
+        mask = (dists >= 0) * (dists < depth_steps) #select potential planes
 
-        fi_idxes_ = fi_idxes[mask]
+        fi_idxes_    = fi_idxes[mask]
         theta_idxes_ = theta_idxes[mask]
-        dists = dists[mask]
+        dists        = dists[mask]
 
+        #tout les phis et thetas qui conviennent à ce point
         accum[fi_idxes_, theta_idxes_, dists] += 1
 
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #on essaie de trouver les meilleurs points de l'accumulateur
     points_best = []
     for i in range(len(fis)):
         for j in range(len(thetas)):
             for k in range(depth_start_step, depth_steps):
                 v = accum[i, j, k]
                 if v >= threshold:
-                    points_best.append([i, j, k, v])
+                    points_best.append([i, j, k, v]) 
+
     points_best = np.array(points_best)
     if len(points_best) == 0:
-        logger.warning('Failed to find hough planes: all points below threshold')
+        logger.warning('Failed to find hough planes: all points below threshold = # element in a point in acumulator')
         return None, None
 
 
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(points_best[:,:3])
+    #eps = Mininal distance between point in cluster
+    #min_points = minimal number of points in a cluster
     cluster_idxes = pcd.cluster_dbscan(eps=dbscan_eps, min_points=dbscan_min_points)
+    labels = np.array( pcd.cluster_dbscan(eps=dbscan_eps, min_points=dbscan_min_points))
+    max_label = labels.max()
+    print(f"point cloud has {max_label + 1} clusters")
+    colors = plt.get_cmap("tab20")(labels / (max_label if max_label > 0 else 1))
+    colors[labels < 0] = 0
+    pcd.colors = o3d.utility.Vector3dVector(colors[:, :3])
+    o3d.visualization.draw_geometries([pcd],
+                                    zoom=0.455,
+                                    front=[-0.4999, -0.1659, -0.8499],
+                                    lookat=[2.1813, 2.0619, 2.0999],
+                                    up=[0.1204, -0.9852, 0.1215])
 
     clusters = {}
     for i in range(len(cluster_idxes)):
@@ -213,6 +256,7 @@ def hough_planes(points, threshold, use_tqdm=True,
             clusters[idx] = []
 
         clusters[idx].append(points_best[i])
+    # print('clusters',clusters[0][0])
 
     if -1 in clusters:
         del clusters[-1]
@@ -224,20 +268,26 @@ def hough_planes(points, threshold, use_tqdm=True,
     planes_out = []
     for k, v in clusters.items():
         logger.debug(f'~~~~~~~~~~~~{k}~~~~~~~~~~~~')
-        cluster = np.array(v, dtype=np.int)
+        cluster = np.array(v, dtype=np.int64)
 
-        coords = cluster[:, :3]
-        weights = cluster[:, 3]
+        coords = cluster[:, :3] #[x,y,z]
+        weights = cluster[:, 3] # value in the accumulator[]
+
+        # \sum_i (xi*wi + yi*wi + zi*wi) / \sum (wi) (coordonée sphérique)
         for i in range(3):
             coords[:, i] *= weights
+
         cluster_size = len(weights)
 
-        coord = np.sum(coords, axis=0) / np.sum(weights)
+        coord = np.sum(coords, axis=0) / np.sum(weights) 
         logger.debug(f'coord={coord}, cluster_size={cluster_size}')
+
+        #recuperer 
 
         fi = (fi_bounds[0] + coord[0]*fi_step) / 180 * np.pi
         theta = (theta_bounds[0] + coord[1]*theta_step) / 180 * np.pi
         depth = (coord[2] + depth_skipped_steps) / depth_total_steps * depth_bounds[1]
+
         point = _fi_theta_depth_to_point(fi, theta,depth)
         logger.debug(f'fi,theta,depth = ({fi},{theta},{depth})')
         logger.debug(f'plane point: {point}')
@@ -247,4 +297,21 @@ def hough_planes(points, threshold, use_tqdm=True,
     planes_out = np.array(planes_out)
     logger.debug(f'~~~~~~~~~~~~~~~~~~~~~~~~')
 
+    for i in len(points_best):
+        points_best[i,:,:]*=(180/np.pi)
+        points_best[:,i,:]*=(180/np.pi)
+
     return planes_out, points_best
+
+
+if __name__ =='__main__':
+    pcd = o3d.io.read_point_cloud(r'TFE-SPOT\hough-plane-python-master\RES\map_go_5.pcd')
+    pcd = pcd.voxel_down_sample(voxel_size=0.1)
+    points = np.asarray(pcd.points)
+    print('Number of points:', len(points))
+
+    planes, points_best = hough_planes(points, threshold=500, fi_step=2, theta_step=2, depth_bounds=(0, 4))
+    print('planes \n',planes)
+    print('point best \n', points_best)
+    viz.visualize_plane(points, planes[:,:3])
+    viz.show_points(points_best, is_hough_space=True)
