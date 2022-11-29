da1560bc8f998f35605b358d0a4d8816c2d1e9d7
DinhThanhPhongDo <50570186+DinhThanhPhongDo@users.noreply.github.com>
2022-11-29 13:02:29 +0100

    Filtrer les outliers

--- PhongyRANSAC.py
+++ PhongyRANSAC.py
@@ -1,29 +1,80 @@
 import numpy as np
-from scipy.linalg import norm
 import open3d as o3d
-import random 
 from tqdm import tqdm
+import time
+# def ransac(pts,pts_n, thresh_d=0.05,thresh_n=0.8,epoch=1000, tqdm_bool=False) :
+#     """
+#     points = np.array(N,3)
+#     """
+#     n_pts    = len(pts)
+#     idx_pts  = np.arange(0,n_pts,1)
+#     best_inlier_mask  = None
+#     best_outlier_mask = None
+#     best_n_inliers = 0
 
-def ransac(pts,pts_n, thresh_d=0.05,thresh_n=0.8,epoch=1000) :
+#     iterator = range(epoch)
+#     if tqdm_bool:
+#         iterator = tqdm(iterator)
+#     for _ in iterator :
+
+#         pts_sample = pts[np.random.choice(idx_pts,3)]
+    
+#         vecA = pts_sample[1, :] - pts_sample[0, :]
+#         vecB = pts_sample[2, :] - pts_sample[0, :]
+
+#         normal      =  np.cross(vecA, vecB)
+#         d           = -np.dot(normal,pts_sample[0,:])
+#         norm_normal = np.linalg.norm(normal)
+
+#         plane = np.array([normal[0], normal[1], normal[2], d])
+    
+#         dist_pt     = np.abs(np.dot(normal[:3],pts.T)+ d) / norm_normal
+
+#         normal = normal/norm_normal
+#         # TODO clustering ?
+
+#         inlier_mask = np.less_equal(dist_pt,thresh_d)*np.greater_equal(np.abs(np.dot(pts_n,normal)),thresh_n)
+#         outlier_mask = np.logical_xor(np.less_equal(dist_pt,thresh_d)*np.greater_equal(np.abs(np.dot(pts_n,normal)),thresh_n))
+#         n_inliers = np.sum(inlier_mask)
+
+#         if n_inliers> best_n_inliers:
+#             best_plane        = plane
+#             best_inlier_mask  = inlier_mask
+#             best_outlier_mask = outlier_mask
+#             best_n_inliers    = n_inliers
+
+#     return best_plane,idx_pts[best_inlier_mask],idx_pts[best_outlier_mask]
+
+
+def ransac(pts,pts_n, thresh_d=0.05,thresh_n=0.8,confidence_interval=0.95,plane_proportion=0.1, tqdm_bool=False) :
     """
     points = np.array(N,3)
     """
     n_pts    = len(pts)
-    idx_pts  = list(np.arange(0,n_pts,1))
-    best_inlier_mask = None
-    n_inliers = 0
+    idx_pts  = np.arange(0,n_pts,1)
+    best_inlier_mask  = None
+    best_outlier_mask = None
+    best_d = None
+    best_n_inliers = 0
+    #https://halshs.archives-ouvertes.fr/halshs-00264843/document
+    eps = 1- plane_proportion
+    alpha = confidence_interval
+    epoch = int(np.log(1-alpha)/np.log(1-np.power(1-eps,3)))
+    print('epoch=',epoch)
 
-
-    for i in tqdm(range(epoch)):
-
-        pts_sample = pts[random.sample(idx_pts,3)]
+    iterator = range(epoch)
+    if tqdm_bool:
+        iterator = tqdm(iterator)
+    for _ in iterator :
+        
+        pts_sample = pts[np.random.choice(idx_pts,3)]
     
         vecA = pts_sample[1, :] - pts_sample[0, :]
         vecB = pts_sample[2, :] - pts_sample[0, :]
 
         normal      =  np.cross(vecA, vecB)
         d           = -np.dot(normal,pts_sample[0,:])
-        norm_normal = norm(normal)
+        norm_normal = np.linalg.norm(normal)
 
         plane = np.array([normal[0], normal[1], normal[2], d])
     
@@ -32,45 +83,73 @@ def ransac(pts,pts_n, thresh_d=0.05,thresh_n=0.8,epoch=1000) :
         normal = normal/norm_normal
         inlier_mask = np.less_equal(dist_pt,thresh_d)*np.greater_equal(np.abs(np.dot(pts_n,normal)),thresh_n)
 
-        if np.sum(inlier_mask)> n_inliers:
+        n_inliers = np.sum(inlier_mask)
+
+        if n_inliers> best_n_inliers:
             best_plane        = plane
             best_inlier_mask  = inlier_mask
-            n_inliers         = np.sum(inlier_mask)
+            best_outlier_mask = np.less_equal(dist_pt,thresh_d)*np.logical_xor(np.less_equal(dist_pt,thresh_d),np.greater_equal(np.abs(np.dot(pts_n,normal)),thresh_n))
+            best_n_inliers    = n_inliers
 
-    return best_plane,best_inlier_mask
+    return best_plane,idx_pts[best_inlier_mask],idx_pts[best_outlier_mask]
 
-def get_all_planes(pts,pts_n,n_plane,i_plane=0):
-    if i_plane==n_plane:
-        return
+def get_all_planes(pcd, voxel_size = 0.1, n_inliers = 100, n_plane=1, display = False):
+    pcd_plane = pcd.voxel_down_sample(voxel_size=voxel_size)
+    pcd_plane.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+    pcd_tree = o3d.geometry.KDTreeFlann(pcd)
     
-    plane,inlier_mask= ransac(pts,pts_n,thresh_n=0.9,thresh_d=0.1,epoch=5000)
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(pts)
-    pts_idx = np.arange(0,len(pts),1)
-    inlier_idx = pts_idx[inlier_mask]
-    inlier_cloud = pcd.select_by_index(inlier_idx)
-    inlier_cloud.paint_uniform_color([0, 1, 0]) #vert
-    outlier_cloud = pcd.select_by_index(inlier_idx, invert=True)
-    outlier_cloud.paint_uniform_color([1, 0, 0]) #rouge
-    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud], zoom=0.8,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
-    print(np.sum(inlier_mask))
 
-    get_all_planes(pts[np.logical_not(inlier_mask)],pts_n[np.logical_not(inlier_mask)],n_plane=n_plane,i_plane=i_plane+1)
+    planes = []
+    inliers_lst = []
+    
 
-    return
+    while(True) :
+        pts_n = np.asarray(pcd_plane.normals)
+        pts   = np.asarray(pcd_plane.points)
+        idx_pts = np.arange(0,len(pts),1)
+        plane,inliers,best_outliers= ransac(pts,pts_n,thresh_n=0.95,thresh_d=0.1,plane_proportion=0.1,confidence_interval=0.95)
+        inliers = idx_pts[inliers]
+        # clustering ? Filtering inliers too far away?
+
+        # mask = np.full(len(best_outliers),False)
+        # for i,j in tqdm(enumerate(best_outliers)):
+        #     [k, idx, _] = pcd_tree.search_radius_vector_3d(pcd.points[j], 0.2)
+        #     p = np.asarray(idx)
+        #     q = np.in1d(np.asarray(idx),inliers)
+        #     r = np.sum(np.in1d(np.asarray(idx),inliers))/len(idx)
+        #     mask[i] = np.sum(np.in1d(np.asarray(idx),inliers))/len(idx)>0.6
+        # mask = np.full(len(best_outliers),False)
+        # inliers = np.append(inliers,best_outliers[mask])
+        # print(np.sum(mask))
+        if len(inliers) < n_inliers :
+            break
+
+        inlier_cloud          = pcd_plane.select_by_index(inliers)
+        inlier_cloud, inlier2 = inlier_cloud.remove_statistical_outlier(nb_neighbors=5,std_ratio=2.0)
+        inlier_cloud.paint_uniform_color([np.random.uniform(0,0.5),np.random.uniform(0,1),np.random.uniform(0,1)]) 
+        inliers = inliers[inlier2]#inliers[inlier2+mask]
+
+        #add outlier that are inliers:
+        
+        outlier_cloud = pcd_plane.select_by_index(inliers, invert=True)
+        outlier_cloud, outlier2 = outlier_cloud.remove_statistical_outlier(nb_neighbors=5,std_ratio=2.0)
+        outlier_cloud.paint_uniform_color([1, 0, 0]) #rouge
+
+        pcd_plane = outlier_cloud
+        inliers_lst.append(inlier_cloud)
+        planes.append(plane)
+    if display :
+        print('number of planes:',len(planes))
+        o3d.visualization.draw_geometries(inliers_lst+[outlier_cloud], zoom=0.8,
+                                    front=[-0.4999, -0.1659, -0.8499],
+                                    lookat=[2.1813, 2.0619, 2.0999],
+                                    up=[0.1204, -0.9852, 0.1215])#+[outlier_cloud]
+    return planes,inliers_lst
 
                                 
 
 
 if __name__ =='__main__':
-    pts = o3d.io.read_point_cloud(r'hough-plane-python-master\RES\10.pcd')
-    pts = pts.voxel_down_sample(voxel_size=0.1)
-    pts.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.2, max_nn=30))
-    pts_n = np.asarray(pts.normals)
-    pts= np.asarray(pts.points)
-
-    get_all_planes(pts,pts_n,n_plane=20)
+    pcd = o3d.io.read_point_cloud("hough-plane-python-master\RES\map_go_5.pcd")    
+    get_all_planes(pcd,n_inliers=1000, n_plane=20, display = True)
 
